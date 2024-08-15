aded67e69134167f07d6a5e54772370a5d692d6f
aniklidetz <aniklidetz@gmail.com>
2024-08-15 08:51:58 +0300

    first commit

---
+++ calc.js
@@ -0,0 +1,92 @@
+let a = ''; // first number
+let b = ''; // second number
+let sign = ''; // operation sign
+let finish  = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+// экран 
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+    a = ''; // first number and result
+    b = ''; // second number 
+    sign = ''; // operation sign
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    //button not pressed
+    if(!event.target.classList.contains('btn')) return;
+    //button pressed clearAll ac
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    //get the pressed button
+    const key = event.target.textContent;
+
+    //if a key from 0-9 or '.' is pressed
+    if (digit.includes(key)) {
+        if (b ==='' && sign === '') {
+            a += key;
+            
+            out.textContent = a;
+            
+        }
+        else if (a!=='' && b!=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b , sign);
+        return;
+    }
+
+     // if a key '+', '-', '/', or '*' is pressed
+     if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b , sign);
+        return;
+    }
+
+    // if '=' is pressed
+    if (key === '=') {
+        if (b ==='') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "X":
+                a = a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Error';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b , sign);
+    }
+
+    
+
+}
